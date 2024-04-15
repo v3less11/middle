7466134bc7c49787d5744e06aec577c26d352ad4
v3less11 <159018254+v3less11@users.noreply.github.com>
2024-04-15 04:01:06 -0700

    Update func4.cpp

--- func4.cpp
+++ func4.cpp
@@ -1,9 +1,14 @@
+#include "middle.h"
+
 long long itc_bin_num(long long number){
+    long long r = 0;
     long long a = 0;
     long long res = 0;
     long long tmp = number;
     while(tmp > 0){
-        a += tmp % 2;
+        int v = tmp%2;
+        r ++;
+        a += v;
         tmp /= 2;
         a*= 10;
     }
@@ -12,9 +17,18 @@ long long itc_bin_num(long long number){
         a /= 10;
         res *= 10;
     }
-    return res / 10;
+    res = res/10;
+    int b = itc_len_num(res);
     
+    if(r > b){
+        while(r > b){
+            res *= 10;
+            b = itc_len_num(res);
+        }
+    }
+    return res;
 }
+
 
 long long itc_oct_num(long long number){
     int a = 0;
